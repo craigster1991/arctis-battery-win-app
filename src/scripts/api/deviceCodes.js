26c4755d9b4efd9774837152d0a859eb43ee3eb8
Craig Wheatley <craigster1991@gmail.com>
2023-08-31 16:51:25 +0100

    adds new devices

--- src/scripts/api/deviceCodes.js
+++ src/scripts/api/deviceCodes.js
@@ -1,28 +1,58 @@
 const deviceCodes = [
 	{
     name: "SteelSeries Arctis 1 Wireless",
-    vendorId: 4152,
-    productId: 4787,
+    vendorId: "4152",
+    productId: "4787",
+  },
+	{
+    name: "SteelSeries Arctis 1 Wireless (Xbox)",
+    vendorId: "4152",
+    productId: "4790",
   },
 	{
     name: "SteelSeries Arctis 7 2017",
-    vendorId: 4152,
-    productId: 4704,
+    vendorId: "4152",
+    productId: "4704",
   },
   {
     name: "SteelSeries Arctis 7 2019",
-    vendorId: 4152,
-    productId: 4781,
+    vendorId: "4152",
+    productId: "4781",
   },
 	{
     name: "SteelSeries Arctis 9",
-    vendorId: 4152,
-    productId: 4802,
+    vendorId: "4152",
+    productId: "4802",
+  },
+	{
+    name: "SteelSeries Arctis 9",
+    vendorId: "4152",
+    productId: "4804",
   },
 	{
     name: "SteelSeries Arctis Pro",
-    vendorId: 4152,
-    productId: 4690,
+    vendorId: "4152",
+    productId: "4690",
+  },
+	{
+    name: "SteelSeries Arctis 7+ (2022)",
+    vendorId: "4152",
+    productId: "8718",
+  },
+  {
+    name: "SteelSeries Arctis Nova Pro Wireless",
+    vendorId: "4152",
+    productId: "4832",
+  },
+  {
+    name: "SteelSeries Arctis Nova 7",
+    vendorId: "4152",
+    productId: "8706",
+  },
+  {
+    name: "SteelSeries Arctis Nova 7x",
+    vendorId: "4152",
+    productId: "8710",
   },
 ]
 

36315e44ab55edd957fec143ad5387ba66f7b2e2
Craig Wheatley <craigster1991@gmail.com>
2023-09-01 17:16:41 +0100

    more debugging data for missing devices

--- src/preload.js
+++ src/preload.js
@@ -3,13 +3,13 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld(
   "api", {
     send: (channel, data) => {
-      const validChannels = ["get-battery", "get-devices"];
+      const validChannels = ["get-battery", "get-devices", "donate"];
       if (validChannels.includes(channel)) {
         ipcRenderer.send(channel, data);
       }
     },
     receive: (channel, cb) => {
-      const validChannels = ["send-battery", "get-devices"];
+      const validChannels = ["send-battery", "get-devices", "donate"];
       if (validChannels.includes(channel)) {
         ipcRenderer.on(channel, (event, ...args) => cb(...args));
       }

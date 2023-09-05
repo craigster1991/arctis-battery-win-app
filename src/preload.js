3d21d0e15eeaa6c59bbe8b3579f8ff8f6a66555d
mehrunes-dagon <mehrunes-dagon@users.noreply.github.com>
2023-09-05 22:06:30 +1000

    add option to hide window by default

--- src/preload.js
+++ src/preload.js
@@ -3,13 +3,13 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld(
   "api", {
     send: (channel, data) => {
-      const validChannels = ["get-battery", "get-devices", "donate"];
+      const validChannels = ["get-battery", "get-devices", "donate", "set-window-default", "get-window-default"];
       if (validChannels.includes(channel)) {
         ipcRenderer.send(channel, data);
       }
     },
     receive: (channel, cb) => {
-      const validChannels = ["send-battery", "get-devices", "donate"];
+      const validChannels = ["send-battery", "get-devices", "donate", "set-window-default", "get-window-default"];
       if (validChannels.includes(channel)) {
         ipcRenderer.on(channel, (event, ...args) => cb(...args));
       }
